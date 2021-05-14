385d58507eb0eccd55c019b4b0ef593c15e574fd
Dina Muktubayeva <dina.muktubayeva@nu.edu.kz>
2021-05-14 19:49:58 +0600

    overflow handling and minor style changes

--- src/App.js
+++ src/App.js
@@ -13,9 +13,13 @@ function App() {
     <div className="App">
       <List list={list} setList={setList} />
       <footer>
-        <p><a href="https://github.com/DinaMuktubayeva/todo-list">Source code</a></p>
-        <p>Author: Dina Muktubayeva</p>        
-        <p>2021</p>        
+        <p>
+          <a href="https://github.com/DinaMuktubayeva/todo-list" target="_blank" rel="noreferrer">
+            Source code
+          </a>
+        </p>
+        <p>Author: Dina Muktubayeva (<a href="mailto:dina.muktubayeva@email.com">mail</a>)</p>
+        <p>2021</p>
       </footer>
     </div>
   );
